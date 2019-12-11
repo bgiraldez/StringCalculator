d85c9bf6ee8445c5bb9d6c50a04929317c86f093
Borja Giraldez Gonzalez <borja.gonzalez@mobgen.com>
2019-12-11 11:35:50 +0100

    Initial Commit

---
+++ StringCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StringCalculator
+//
+//  Created by Borja Giraldez Gonzalez on 11/12/2019.
+//  Copyright © 2019 Borja Giraldez Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
