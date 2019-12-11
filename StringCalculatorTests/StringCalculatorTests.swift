0445a06cd1d9ca96dc9fccaf2a3dca8dd1bdf7fa
Borja Giraldez Gonzalez <borja.gonzalez@mobgen.com>
2019-12-11 14:49:14 +0100

    created first test

--- StringCalculatorTests/StringCalculatorTests.swift
+++ StringCalculatorTests/StringCalculatorTests.swift
@@ -9,26 +9,42 @@
 import XCTest
 @testable import StringCalculator
 
+enum NegativeError: Error {
+    case negativeNotAllowed
+}
 class StringCalculatorTests: XCTestCase {
+    
 
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    let scenarios = [("",0),("1",1),("1,2",3),("1\n2",3),("11,22,33",66)]
+    
+    
+//    func testScenarios() {
+//        scenarios.forEach { (input, expected) in
+//            let result = add(numbers: input)
+//
+//            XCTAssertEqual(expected, result)
+//        }
+//    }
+    
+    func testNegativeNumbers () {
+        let result = try! add(numbers: "-1,1")
+        
+        XCTAssertEqual(0, result)
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+     func add (numbers: String) throws -> Int {
+        guard !numbers.isEmpty else {
+            return 0
         }
-    }
+        let arrayNumbers: [String] = numbers.components(separatedBy: [",","\n"])
+        var sum = 0
+        for number in arrayNumbers {
+            if (Int(number)! < 0) {
+                throw NegativeError.negativeNotAllowed
+            }
+            sum += Int(number)!
+        }
+        return sum
+      }
 
 }
